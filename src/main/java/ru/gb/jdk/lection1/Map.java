4d70f01215779ef597c9f37d4d2aff8cdb9ae7b1
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-26 13:19:26 +0300

    Feat: class Map paint game field

--- src/main/java/ru/gb/jdk/lection1/Map.java
+++ src/main/java/ru/gb/jdk/lection1/Map.java
@@ -4,11 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Map extends JPanel {
+    private int panelWidth;
+    private int panelHeight;
+    private int cellWidth;
+    private int cellHeight;
+
     Map() {
     }
 
     void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
         System.out.printf("Mode: %d;\nSize: x=%d, y=%d\nWin Length: %d", mode, fSzX, fSzY, wLen);
+        repaint();
     }
 
     @Override
@@ -18,9 +24,19 @@ public class Map extends JPanel {
     }
 
     private void render(Graphics g) {
+        panelWidth = getWidth();
+        panelHeight = getHeight();
+        cellWidth = panelWidth / 3;
+        cellHeight = panelHeight / 3;
+
         g.setColor(Color.BLACK);
-        g.drawLine(0, 0, 100, 100);
-        int panelWidth = getWidth();
-        int panelHeight = getHeight();
+        for (int h = 0; h < 3; h++) {
+            int y = h * cellHeight;
+            g.drawLine(0, y, panelWidth, y);
+        }
+        for (int w = 0; w < 3; w++) {
+            int x = w * cellWidth;
+            g.drawLine(x, 0, x, panelHeight);
+        }
     }
 }

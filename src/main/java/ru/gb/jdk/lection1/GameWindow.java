b485100fd095a81f8bbd81a001270df713bba243
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-26 13:05:12 +0300

    Feat: button listeners

--- src/main/java/ru/gb/jdk/lection1/GameWindow.java
+++ src/main/java/ru/gb/jdk/lection1/GameWindow.java
@@ -48,4 +48,8 @@ public class GameWindow extends JFrame {
         add((map));
         setVisible(true);
     }
+
+    void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
+        map.startNewGame(mode, fSzX, fSzY, wLen);
+    }
 }
--- src/main/java/ru/gb/jdk/lection1/SettingsWindow.java
+++ src/main/java/ru/gb/jdk/lection1/SettingsWindow.java
@@ -1,6 +1,8 @@
 package ru.gb.jdk.lection1;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SettingsWindow extends JFrame {
     private static final int WINDOW_WIDTH = 350;
@@ -11,6 +13,13 @@ public class SettingsWindow extends JFrame {
     SettingsWindow(GameWindow gameWindow) {
         setLocationRelativeTo(gameWindow);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                gameWindow.startNewGame(0, 3, 3, 3);
+                setVisible(true);
+            }
+        });
 
         add(btnStart);
     }

096957af3304bec0b72da9439dd7fcadc531bcd5
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-26 12:13:02 +0300

    Feat: class GameWindow main window

--- src/main/java/ru/gb/jdk/lection1/GameWindow.java
+++ src/main/java/ru/gb/jdk/lection1/GameWindow.java
@@ -3,7 +3,16 @@ package ru.gb.jdk.lection1;
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
+    public static final int WINDOW_WIDTH = 507;
+    public static final int WINDOW_HEIGHT = 555;
+    public static final int WINDOW_POSX = 800;
+    public static final int WINDOW_POSY = 300;
+
     GameWindow() {
-        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(WINDOW_POSX, WINDOW_POSY);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        setVisible(true);
     }
 }

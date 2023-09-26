18eaca326f449cbf7619fd470111b413383ca74f
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-26 12:53:37 +0300

    Feat: class SettingsWindow

--- src/main/java/ru/gb/jdk/lection1/SettingsWindow.java
+++ src/main/java/ru/gb/jdk/lection1/SettingsWindow.java
@@ -3,7 +3,15 @@ package ru.gb.jdk.lection1;
 import javax.swing.*;
 
 public class SettingsWindow extends JFrame {
-    SettingsWindow(GameWindow gameWindow) {
+    public static final int WINDOW_WIDTH = 350;
+    public static final int WINDOW_HEIGHT = 230;
 
+    JButton btnStart = new JButton("Start new game");
+
+    SettingsWindow(GameWindow gameWindow) {
+        setLocationRelativeTo(gameWindow);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        add(btnStart);
     }
 }

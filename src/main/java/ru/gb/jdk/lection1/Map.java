4992126f653151d85de9ac1ef7e6a580a7dd3960
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-26 12:43:29 +0300

    Feat: class Map

--- src/main/java/ru/gb/jdk/lection1/Map.java
+++ src/main/java/ru/gb/jdk/lection1/Map.java
@@ -7,4 +7,8 @@ public class Map extends JPanel {
     Map() {
         setBackground(Color.BLACK);
     }
+
+    void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
+        System.out.printf("Mode: %d;\nSize: x=%d, y=%d\nWin Length: %d", mode, fSzX, fSzY, wLen);
+    }
 }

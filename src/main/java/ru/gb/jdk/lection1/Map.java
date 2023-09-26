5ac8b004dffb61e347f222900ce9e3fb8c4d1b7a
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-26 13:12:48 +0300

    Feat: class Map paintComponent

--- src/main/java/ru/gb/jdk/lection1/Map.java
+++ src/main/java/ru/gb/jdk/lection1/Map.java
@@ -5,10 +5,22 @@ import java.awt.*;
 
 public class Map extends JPanel {
     Map() {
-        setBackground(Color.BLACK);
     }
 
     void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
         System.out.printf("Mode: %d;\nSize: x=%d, y=%d\nWin Length: %d", mode, fSzX, fSzY, wLen);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render(g);
+    }
+
+    private void render(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.drawLine(0, 0, 100, 100);
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
     }
 }

05b59fa10e90408b1724bc0dfeea5a5c6fb2d413
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-26 13:22:14 +0300

    Feat: class Map mouseListener

--- src/main/java/ru/gb/jdk/lection1/Map.java
+++ src/main/java/ru/gb/jdk/lection1/Map.java
@@ -2,6 +2,8 @@ package ru.gb.jdk.lection1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Map extends JPanel {
     private int panelWidth;
@@ -10,7 +12,18 @@ public class Map extends JPanel {
     private int cellHeight;
 
     Map() {
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                update(e);
+            }
+        });
     }
+
+    private void update(MouseEvent e) {
+        repaint();
+    }
+
 
     void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
         System.out.printf("Mode: %d;\nSize: x=%d, y=%d\nWin Length: %d", mode, fSzX, fSzY, wLen);

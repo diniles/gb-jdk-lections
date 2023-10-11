8bcf2722789cf997a4f0c9c842d44f9751bef8b1
Dzmitry Liashko <dzliashko@gmail.com>
2023-10-11 15:37:54 +0300

    Feat: lection 3 class GBox

---
+++ src/main/java/ru/gb/jdk/lection3/GBox.java
@@ -0,0 +1,21 @@
+package ru.gb.jdk.lection3;
+
+public class GBox<T> {
+    private T value;
+
+    public GBox(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void showType() {
+        System.out.printf("Type is %s, which value is %s\n", value.getClass().getName(), getValue());
+    }
+}

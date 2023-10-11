1bde9b93d4aaa0cac71ce5f4786032583b4eeb11
Dzmitry Liashko <dzliashko@gmail.com>
2023-10-11 15:40:24 +0300

    Feat: lection 3 class Main

--- src/main/java/ru/gb/jdk/lection3/Main.java
+++ src/main/java/ru/gb/jdk/lection3/Main.java
@@ -3,13 +3,10 @@ package ru.gb.jdk.lection3;
 public class Main {
 
     public static void main(String[] args) {
-        Box b1 = new Box(20);
-        Box b2 = new Box(30);
-        System.out.println((Integer) b1.getObj() + (Integer) b2.getObj());
-
-        Box b3 = new Box("Hello, ");
-        Box b4 = new Box("World!");
-        System.out.println(b3.getObj() + (String) b4.getObj());
+        GBox<String> stringBox = new GBox<>("Hello!");
+        stringBox.showType();
+        GBox<Integer> integerBox = new GBox<>(12);
+        integerBox.showType();
     }
 }
 
